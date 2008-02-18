60e678288bdfcd17fc9543465b847bb35a933138
Fabio Mascarenhas <mascarenhas@acm.org>
2008-02-18 18:29:09 +0000

    tweak for darwin ppc
    20080218182909-678b8-aec1cc443c207b4868118d384b8ac0857116364e.gz

--- src/alien/core.c
+++ src/alien/core.c
@@ -293,6 +293,10 @@ static void *alien_loadfunc (lua_State *L, void *lib, const char *sym) {
 #define FFI_STDCALL FFI_DEFAULT_ABI
 #endif
 
+#ifdef DARWIN
+#define FFI_SYSV FFI_DEFAULT_ABI
+#endif
+
 static const ffi_abi ffi_abis[] = { FFI_DEFAULT_ABI, FFI_SYSV, FFI_STDCALL };
 static const char *const ffi_abi_names[] = { "default", "cdecl", "stdcall", NULL };
 
